02a24a2a000d9fbd428027686acea6044ced5c2b
John Seekins <john@civiceagle.com>
2022-03-30 15:19:33 -0600

    add build script
    Signed-off-by: John Seekins <john@civiceagle.com>

---
+++ util/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TAG=$(date +%s)
+
+docker build --tag us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG .
+docker tag us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
+docker push us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG
+docker push us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
