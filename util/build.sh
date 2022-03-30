3e3aade853c8fa025f5afde1c3de7f67573c5e0d
John Seekins <john@civiceagle.com>
2022-03-30 15:20:31 -0600

    fix linting
    Signed-off-by: John Seekins <john@civiceagle.com>

--- util/build.sh
+++ util/build.sh
@@ -2,7 +2,7 @@
 
 TAG=$(date +%s)
 
-docker build --tag us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG .
-docker tag us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
-docker push us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG
+docker build --tag "us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG" .
+docker tag "us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG" us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
+docker push "us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG"
 docker push us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
