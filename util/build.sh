ba9643f0dc3bddb118aa8f283b39967c12aee482
John Seekins <john@civiceagle.com>
2022-05-17 17:03:33 -0600

    fix build (part 3)
    Signed-off-by: John Seekins <john@civiceagle.com>

--- util/build.sh
+++ util/build.sh
@@ -4,7 +4,7 @@ TAG=$(date +%s)
 ROOT="gcr.io"
 PROJECT="civic-eagle-enview-dev"
 
-docker build --tag "${ROOT}/${PROJECT}//github-stat-collector:$TAG" .
+docker build --tag "${ROOT}/${PROJECT}/github-stat-collector:$TAG" .
 docker tag "${ROOT}/${PROJECT}/github-stat-collector:$TAG" "${ROOT}/${PROJECT}/github-stat-collector:latest"
 docker push "${ROOT}/${PROJECT}/github-stat-collector:$TAG"
 docker push "${ROOT}/${PROJECT}/github-stat-collector:latest"
