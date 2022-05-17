7f67427d4a4fda0711a39d0f112217f0c92bda1f
John Seekins <john@civiceagle.com>
2022-05-17 16:34:29 -0600

    fix build script
    Signed-off-by: John Seekins <john@civiceagle.com>

--- util/build.sh
+++ util/build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 TAG=$(date +%s)
+ROOT="us-central1-docker.pkg.dev"
+PROJECT="civic-eagle-enview-dev"
+REPO="docker"
 
-docker build --tag "us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG" .
-docker tag "us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG" us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
-docker push "us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:$TAG"
-docker push us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
+docker build --tag "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:$TAG" .
+docker tag "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:$TAG" us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
+docker push "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:$TAG"
+docker push "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:latest"
