85e1af5083a89481e29d1f9d538465e6a3f54355
John Seekins <john@civiceagle.com>
2022-05-17 17:02:40 -0600

    fix build script to use gcr.io repo
    Signed-off-by: John Seekins <john@civiceagle.com>

--- util/build.sh
+++ util/build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 TAG=$(date +%s)
-ROOT="us-central1-docker.pkg.dev"
+ROOT="gcr.io"
 PROJECT="civic-eagle-enview-dev"
-REPO="docker"
 
-docker build --tag "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:$TAG" .
-docker tag "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:$TAG" us-docker.pkg.dev/civic-eagle-enview-dev/civiceagle/github-stat-collector:latest
-docker push "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:$TAG"
-docker push "${ROOT}/${PROJECT}/${REPO}/github-stat-collector:latest"
+docker build --tag "${ROOT}/${PROJECT}//github-stat-collector:$TAG" .
+docker tag "${ROOT}/${PROJECT}/github-stat-collector:$TAG" "${ROOT}/${PROJECT}/github-stat-collector:latest"
+docker push "${ROOT}/${PROJECT}/github-stat-collector:$TAG"
+docker push "${ROOT}/${PROJECT}/github-stat-collector:latest"
