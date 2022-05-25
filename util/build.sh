85134313d9831818ca8ad7e448b2e95a69d90d4a
John Seekins <john@civiceagle.com>
2022-05-25 17:12:07 -0600

    local building should also lint
    Signed-off-by: John Seekins <john@civiceagle.com>

--- util/build.sh
+++ util/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-TAG=${TAG:-$(git tag | tail -1)}
 ROOT="gcr.io"
 PROJECT="civic-eagle-enview-dev"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd "${SCRIPT_DIR}/../" >/dev/null 2>&1 || exit 1
+TAG=${TAG:-$(git tag | tail -1)}
+
+docker run --rm -v "${SCRIPT_DIR}/../:/apps" alpine/flake8:latest .
+docker run --rm -v "${SCRIPT_DIR}/../:/data" cytopia/black:latest .
 
 docker build --tag "${ROOT}/${PROJECT}/github-stat-collector:$TAG" .
 docker tag "${ROOT}/${PROJECT}/github-stat-collector:$TAG" "${ROOT}/${PROJECT}/github-stat-collector:latest"
