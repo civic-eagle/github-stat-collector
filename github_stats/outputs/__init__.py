7b6391e3828fe155c1362e5bf956a52143e7b1a8
John Seekins <john@civiceagle.com>
2022-06-29 14:50:27 -0600

    fix formatting
    Signed-off-by: John Seekins <john@civiceagle.com>

--- github_stats/outputs/__init__.py
+++ github_stats/outputs/__init__.py
@@ -92,7 +92,9 @@ class StatsOutput(object):
         stat["name"] = "avg_windowed_commit_release_time_secs"
         stat["measurement_type"] = "gauge"
         stat["value"] = commits["windowed_commit_time"]
-        stat["description"] = "Average time between commit and release within defined collection window"
+        stat[
+            "description"
+        ] = "Average time between commit and release within defined collection window"
         formatted_stats.append(stat)
         stat = deepcopy(self.tmpobj)
         stat["name"] = "unreleased_commits_count"
